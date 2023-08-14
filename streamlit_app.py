7de70903c9c9254490e414d53e063f2e4ab1ea6b
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-08-15 02:38:21 +0900

    column_config

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,19 @@ try:
         use_container_width=True,
         hide_index=True,
         column_config={
-            'itmsNm': '채권명'
-        }
+            'itmsNm': '채권명',
+            'trqu': '거래량',
+            'clprPrc': '가격',
+            'clprBnfRt': '수익률',
+            'bondSrfcInrt': '표면이율'
+            'scrsItmsKcdNm': '분류',
+            'bondIsurNm': '발행',
+            'bondExprDt': '만기일',
+            'intPayCyclCtt': '이자주기',
+            'kisScrsItmsKcdNm': '한국신용평가',
+            'kbpScrsItmsKcdNm': '한국자산평가',
+            'niceScrsItmsKcdNm': 'NICE평가정보'
+        },
     )
 except:
     st.info('🫠 데이터가 없습니다')
