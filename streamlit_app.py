8209425702d3e4480cdde8de97f6b2b73e9e7695
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-08-15 02:52:26 +0900

    rating

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,7 @@ import data
 st.title('🏃 채권 모아보기')
 
 dt = st.date_input('🗓️ 조회할 일자')
-
-try:
-    df = data.get_bond_info(dt.strftime('%Y%m%d'))
-    df.trqu = df.trqu.astype('int')
-    st.dataframe(df,
-        use_container_width=True,
-        hide_index=True,
-        column_config={
+column_config = {
             'itmsNm': '채권명',
             'trqu': '거래량',
             'clprPrc': '가격',
@@ -21,10 +14,28 @@ try:
             'bondIsurNm': '발행',
             'bondExprDt': '만기일',
             'intPayCyclCtt': '이자주기',
-            'kisScrsItmsKcdNm': '한국신용평가',
-            'kbpScrsItmsKcdNm': '한국자산평가',
-            'niceScrsItmsKcdNm': 'NICE평가정보',
-        },
+            # 'kisScrsItmsKcdNm': '한국신용평가',
+            # 'kbpScrsItmsKcdNm': '한국자산평가',
+            # 'niceScrsItmsKcdNm': 'NICE평가정보',
+            'rating': '신용도',
+        }
+
+try:
+    df = data.get_bond_info(dt.strftime('%Y%m%d'))
+    df.trqu = df.trqu.astype('int')
+    df.kisScrsItmsKcdNm = df.kisScrsItmsKcdNm.str.replace('0', '') 
+    df.niceScrsItmsKcdNm = df.kisScrsItmsKcdNm.str.replace('0', '') 
+    df.bondExprDt = pd.to_datetime(
+        df.bondExprDt.apply(str),
+        format='%Y%m%d', errors='coerce').dt.date
+    df.rating = df.apply(lambda x: '/'.join(set([str(y).replace('0', '')
+                for y in (x.kisScrsItmsKcdNm, x.kbpScrsItmsKcdNm, x.niceScrsItmsKcdNm)
+                if str(y) != 'nan'])), axis=1)
+    st.dataframe(df.iloc[:,
+        [0,4,5,1,3,7,6,8,12]],
+        use_container_width=True,
+        hide_index=True,
+        column_config=column_config,
     )
 except:
     st.info('🫠 데이터가 없습니다')
