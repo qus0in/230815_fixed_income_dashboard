1a676af11b6bcaaf8b032c64f37f27a3d1f53e23
qus0in <qus0in@gmail.com>
2023-08-16 15:56:37 +0900

    필터 업그레이드

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import data
+import datetime
+import pandas as pd
 
 st.title('🏃 채권 모아보기')
 
 dt = st.date_input('🗓️ 조회할 일자')
+st.toast('T+1 09:00 이후에 업데이트', icon='🗓️')
 column_config = {
             'itmsNm': '채권명',
             'trqu': '거래량',
@@ -25,7 +28,7 @@ try:
     df = data.get_bond_info(dt.strftime('%Y%m%d'))
     with st.container():
         st.subheader('🌱 필터')
-        col1, col2, col3  = st.columns(3)
+        col1, col2, col3, col4  = st.columns(4)
         with col1:
             r = st.radio('🌶️ 거래량', ['없음', '50% 이상', '75% 이상'])
             if r != '없음':
@@ -35,17 +38,26 @@ try:
             if invest == '적용':
                 df.query(f'not rating.str.contains("B") or rating.str.contains("BBB")', inplace=True)
         with col3:
-            ms = st.multiselect(
-                '🍅 종류', ['후순위', '신종자본', '조건부자본', '전환사채'],
-                default=['후순위', '신종자본', '조건부자본', '전환사채'])
-            if '후순위' not in ms:
-                df.query('not itmsNm.str.contains("\(후\)") ', inplace=True)
-            if '신종자본' not in ms:
-                df.query('not itmsNm.str.contains("신종") ', inplace=True)
-            if '조건부자본' not in ms:
-                df.query('not itmsNm.str.contains("\(상\)") ', inplace=True)
-            if '전환사채' not in ms:
-                df.query('not itmsNm.str.contains("CB") ', inplace=True)
+            duration = st.radio('🥃 최대 듀레이션', ['없음', '3년'])
+            filter_date = (pd.Timestamp(datetime.datetime.today()) + pd.DateOffset(years=3)).date()
+            if duration == '3년':
+                df.query(f'bondExprDt < @filter_date',
+                         inplace=True)
+        with col4:
+            earn = st.radio('🥃 최소 기대수익률', ['없음', '5%'])
+            if earn == '5%':
+                df.query(f'afterTax >= 5', inplace=True)
+        ms = st.multiselect(
+            '🍅 종류', ['후순위', '신종자본', '조건부자본', '전환사채'],
+            default=['후순위', '신종자본', '조건부자본', '전환사채'])
+        if '후순위' not in ms:
+            df.query('not itmsNm.str.contains("\(후\)") ', inplace=True)
+        if '신종자본' not in ms:
+            df.query('not itmsNm.str.contains("신종") ', inplace=True)
+        if '조건부자본' not in ms:
+            df.query('not itmsNm.str.contains("\(상\)") ', inplace=True)
+        if '전환사채' not in ms:
+            df.query('not itmsNm.str.contains("CB") ', inplace=True)
 
     st.dataframe(df,
         use_container_width=True,
